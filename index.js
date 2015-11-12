a2ea3730ab1de2b3097d70e46305fb5e91b80b38
Ian Patton <ian.patton@gmail.com>
2015-11-12 14:54:41 -0500

    fix index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  ByrdServerTransport: require('./lib/server'),
+  ByrdServerTransport: require('./lib/transport'),
   ByrdServerContact: require('./lib/contact'),
   Router: require('./lib/router'),
   log: require('./lib/log')
