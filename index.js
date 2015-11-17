5786cf1a3bf09bd2dfde51c0069e88b94133bcad
Gordon Hall <gordon@gordonwritescode.com>
2015-11-17 18:44:28 -0500

    clean up logger

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ module.exports.ClientTransport = require('./lib/client-transport');
 module.exports.Contact = require('./lib/contact');
 module.exports.Router = require('./lib/router');
 module.exports.Shredder = require('./lib/shredder');
+module.exports.Egg = require('./lib/egg');
+module.exports.logger = require('./lib/log');
